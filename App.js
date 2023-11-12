fd009947cd04a858f9caa1fba60ad64006bad3dc
Amr Elhady <theamrelhady@gmail.com>
2023-11-12 08:55:18 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './App.css';
+import treeIcon from './treeIcon.png'
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [generatedText, setGeneratedText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [conversation, setConversation] = useState([]); // Store the entire conversation
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('http://localhost:9000/generate-text', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: inputText }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setConversation([...conversation, { role: 'user', content: inputText }, { role: 'assistant', content: data.generatedText }]); // Append both user and AI responses to the conversation
+      setInputText(''); // Clear the input for a new message
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+      setGeneratedText("Failed to generate text."); // Provide feedback on failure
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={treeIcon} alt="GPTree icon" className="App-logo" />
+        <h1>GPTree</h1>
+        <p>A tool for writers to plan their ideas with AI.</p>
+      </header>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          disabled={isLoading}
+          placeholder="Enter your prompt"
+        />
+        <button
+          onClick={handleSubmit}
+          disabled={isLoading}
+        >
+          {isLoading ? 'Generating...' : 'Generate'}
+        </button>
+      </div>
+      <div className="messages">
+        {conversation.map((message, index) => (
+          <p key={index} className={message.role === 'user' ? 'userText' : 'generatedText'}>
+            {message.content}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { OpenAI } = require('openai');
+
+const app = express();
+const port = 9000;
+
+// Use CORS middleware before defining routes to ensure it applies to all of them
+app.use(cors());
+
+// Support JSON payloads
+app.use(express.json());
+
+// Initialize OpenAI API client with your API key
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+console.log(openai);
+
+// Routes
+
+// Define the POST endpoint
+app.post('/generate-text', async (req, res) => {
+    const userInput = req.body.text;
+
+    try {
+        // Make a call to the OpenAI API using the chat completion method
+        const gptResponse = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo", // Keep the model if using the chat completion endpoint
+            messages: [{ role: "user", content: userInput }], // Format for chat completion
+            // You can add other parameters as needed, similar to the completions endpoint
+        });
+
+        // Extract the text from the response
+        //const generatedText = gptResponse.data.choices[0].message.content.trim();
+        console.log(JSON.stringify(gptResponse));
+
+        const generatedText = gptResponse.choices[0].message.content.trim();
+
+        // Send back the generated text as the response
+        res.json({ generatedText });
+    } catch (error) {
+        console.error("Error with OpenAI API:", error);
+        res.status(500).json({ message: "Error generating text" });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
